e39c76917b4619cbc66930d5688f5092b7ac4ea9
Elida2000 <72025704+Elida2000@users.noreply.github.com>
2020-10-15 10:37:38 -0600

    Add files via upload

---
+++ clave_x.py
@@ -0,0 +1,67 @@
+import math
+
+number1 = 2
+number2 = 4
+number3 = 5
+
+
+"""
+***************************************************************
+@@ ejercicio 1 @@
+un metodo python que haga la suma de 2 numeros
+2+4 = 6
+"""
+
+
+# start-->
+def suma():
+    result = number1 + number2
+    return result
+
+
+"""
+***************************************************************
+@@ ejercicio 2 @@
+un metodo python que haga la multiplicacion de 3 numeros
+2*4*5 = 40
+"""
+
+
+# start-->
+def multiplicacion():
+    result = number1 * number2 * number3
+    return result
+
+
+"""
+***************************************************************
+@@ ejercicio 3 @@
+un metodo python que haga la suma de los numeros de la lista
+[2, 5, 4, 6, 9, 12]
+"""
+
+
+# start-->
+
+
+def sumarLista():
+    result = sum(numerosLista)
+    return result
+
+
+numerosLista = [2, 5, 4, 6, 9, 12]
+
+
+"""
+***************************************************************
+@@ ejercicio 4 @@
+colocar este proyecto en github
+colocar aca debajo la url
+enviar la url al correo balbino_a@hotmail.com
+"""
+
+# github url-->
+
+
+def getGithubUrl():
+    return "https://github.com/Elida2000/Tareas_de_Python.git"
